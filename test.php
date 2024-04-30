4a5ea870ba758519d57d344db8ba419c7a9b1294
Astroes-Labs <astroeslabs@gmail.com>
2024-04-30 22:15:51 +0100

    updated test.php with git instructions

--- test.php
+++ test.php
@@ -2,3 +2,5 @@ Lets go
 git remote add origin https://github.com/Astroes-Labs/digitaleden.git
 git branch -M main
 git push -u origin main
+if push origin does work, run the below before running push
+git pull origin main --allow-unrelated-histories
